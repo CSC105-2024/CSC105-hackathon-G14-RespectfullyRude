8d2162a119aaa753278321e3190148e9fe2b2a04
khinnadiko <khin.nadi1@mail.kmutt.ac.th>
2025-06-01 06:01:19 +0700

    Revert "search mobile"
    This reverts commit f1e3b758ebd62debf29ede3c188596444296a174.

--- respectfully-rude-frontend/src/pages/HomePage.jsx
+++ respectfully-rude-frontend/src/pages/HomePage.jsx
@@ -42,7 +42,7 @@ const HomePage = () => {
   const navigate = useNavigate();
   return (
     <div className="flex flex-col items-center justify-around py-5">
-      <div className="flex flex-col md:flex-row mb-15 gap-2 items-center md:justify-around w-screen">
+      <div className="hidden md:flex mb-15 justify-around w-screen">
         <div className="flex items-center border border-[var(--color-foreground)] px-3 w-100 rounded-md hover:border-black h-9">
           <Input
             type="text"
