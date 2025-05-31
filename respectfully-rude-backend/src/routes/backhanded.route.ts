182e769084abd37a91862ad463c79c09b89aa5be
Slade <saib6064@gmail.com>
2025-05-31 19:25:17 +0700

    feat(backhanded.route.ts): implement create and edit routes for backhanded compliments feat(backhanded.route.ts): add authentication middleware to secure routes

--- respectfully-rude-backend/src/routes/backhanded.route.ts
+++ respectfully-rude-backend/src/routes/backhanded.route.ts
@@ -1,8 +1,12 @@
-// import { Hono } from "hono";
-// import { createList } from "../controllers/backhanded/create.backhanded.ts";
+import { Hono } from "hono";
+import { createList } from "../controllers/backhanded/create.backhanded.ts";
+import { verify } from "../middlewares/verify.ts";
+import { editList } from "../controllers/backhanded/edit.backhanded.ts";
 
-// const backhandedRouter = new Hono();
+const backhandedRouter = new Hono();
 
-// backhandedRouter.post("/create", createList);
+backhandedRouter.use(verify);
+backhandedRouter.post("/create", createList);
+backhandedRouter.put("/edit/:id", editList);
 
-// export { backhandedRouter };
+export { backhandedRouter };
