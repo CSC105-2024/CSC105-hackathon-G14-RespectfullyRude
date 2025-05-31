837e60f9a2d86db26cf4b1c8875f9775b15fd2ee
khinnadiko <khin.nadi1@mail.kmutt.ac.th>
2025-05-31 09:20:03 +0700

    Initial frontend setup

---
+++ respectfully-rude-frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+
+const App = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
