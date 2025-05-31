ae0d42721221b56ada9c357ba8fe567488b27757
Slade <saib6064@gmail.com>
2025-05-31 20:12:10 +0700

    feat(backhanded.route.ts): add toggle list route
    This route allows toggling the state of a backhanded list item.
    It uses the PATCH method on the `/toggle/:id` endpoint and calls
    the `toggleList` controller function to handle the logic.

--- respectfully-rude-backend/src/routes/backhanded.route.ts
+++ respectfully-rude-backend/src/routes/backhanded.route.ts
@@ -4,6 +4,7 @@ import { verify } from "../middlewares/verify.ts";
 import { editList } from "../controllers/backhanded/edit.backhanded.ts";
 import { deleteList } from "../controllers/backhanded/delete.backhanded.ts";
 import { getLists } from "../controllers/backhanded/get.backhanded.ts";
+import { toggleList } from "../controllers/backhanded/toggle.backhanded.ts";
 
 const backhandedRouter = new Hono();
 
@@ -12,5 +13,6 @@ backhandedRouter.post("/create", createList);
 backhandedRouter.put("/edit/:id", editList);
 backhandedRouter.delete("/delete/:id", deleteList);
 backhandedRouter.get("/get", getLists);
+backhandedRouter.patch("/toggle/:id", toggleList);
 
 export { backhandedRouter };
