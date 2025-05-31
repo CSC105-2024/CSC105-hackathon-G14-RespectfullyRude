efd3c9f1d7a90073a626d1f393a0b9475cd442f2
khinnadiko <khin.nadi1@mail.kmutt.ac.th>
2025-05-31 09:50:33 +0700

    Set up folders

--- respectfully-rude-frontend/src/pages/LoginPage.jsx
+++ respectfully-rude-frontend/src/pages/LoginPage.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  return (
+    <div className="flex min-w-screen min-h-screen justify-center items-center">
+      <div className="left">
+        <h1 className="text-3xl font-bold">Welcome back</h1>
+        <div className="flex flex-col items-center">
+        <form>
+            
+        </form>
+        </div>
+      </div>
+      <div className="right">right</div>
+    </div>
+  );
 };
 
 export default LoginPage;
