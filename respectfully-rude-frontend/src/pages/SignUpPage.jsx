371a0749ab6e656f771978ba376c863f9e094741
Slade <saib6064@gmail.com>
2025-06-01 00:08:46 +0700

    feat(SignUpPage.jsx): implement form validation using Zod and react-hook-form feat(SignUpPage.jsx): integrate useRegister hook to handle user registration feat(SignUpPage.jsx): display validation errors to the user

--- respectfully-rude-frontend/src/pages/SignUpPage.jsx
+++ respectfully-rude-frontend/src/pages/SignUpPage.jsx
@@ -5,9 +5,47 @@ import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
 import FormInput from "../components/FormInput";
 import { Link } from "react-router-dom";
 import { Button } from "@/components/ui/button";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import useRegister from "@/hooks/useRegister";
 
 const SignUpPage = () => {
   const [showPassword, setShowPassword] = useState(false);
+
+  const signupSchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6, "Password must be at least 6 characters long"),
+    confirmPassword: z
+      .string()
+      .min(6, "Password must be at least 6 characters long"),
+  });
+
+  const { registerUser } = useRegister();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(signupSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    },
+  });
+
+  const onSubmit = async (data) => {
+    await registerUser(
+      data.name,
+      data.email,
+      data.password,
+      data.confirmPassword
+    );
+  };
 
   return (
     <div className="grid lg:grid-cols-2 min-w-screen min-h-screen justify-center items-center">
@@ -15,7 +53,6 @@ const SignUpPage = () => {
         <img src={signUpPhoto} alt="Image error" className="w-45" />
         <h1 className="text-3xl font-extrabold text-[var(--color-primary)]">
           {" "}
-          {/* fix font */}
           Respectfully Rude
         </h1>
         <h2 className="font-normal md:w-80 lg:w-full text-center text-base text-[var(--color-primary)]">
@@ -27,19 +64,30 @@ const SignUpPage = () => {
       <div className="right">
         <h2 className="font-bold text-center text-2xl">Create Account</h2>
         <div className="flex flex-col items-center">
-          <form className="flex flex-col items-center">
+          <form
+            className="flex flex-col items-center"
+            onSubmit={handleSubmit(onSubmit)}
+          >
             <FormInput
               label={"Name"}
               icon={<User size={18} />}
               type={"text"}
               placeholder={"Enter your name"}
+              {...register("name")}
             />
+            {errors.name && (
+              <p className="text-red-500 text-sm">{errors.name.message}</p>
+            )}
             <FormInput
               label={"Email"}
               icon={<Mail size={18} />}
               type={"text"}
               placeholder={"Enter your email"}
+              {...register("email")}
             />
+            {errors.email && (
+              <p className="text-red-500 text-sm">{errors.email.message}</p>
+            )}
             <FormInput
               label={"Password"}
               icon={<Lock size={18} />}
@@ -49,7 +97,11 @@ const SignUpPage = () => {
               onToggle={() => {
                 setShowPassword(!showPassword);
               }}
+              {...register("password")}
             />
+            {errors.password && (
+              <p className="text-red-500 text-sm">{errors.password.message}</p>
+            )}
             <FormInput
               label={"Confirm Password"}
               icon={<Lock size={18} />}
@@ -59,7 +111,13 @@ const SignUpPage = () => {
               onToggle={() => {
                 setShowPassword(!showPassword);
               }}
+              {...register("confirmPassword")}
             />
+            {errors.confirmPassword && (
+              <p className="text-red-500 text-sm">
+                {errors.confirmPassword.message}
+              </p>
+            )}
             <Button
               className="text-base bg-[var(--color-primary)] w-1/3 cursor-pointer hover:bg-[var(--color-secondary)] mt-2"
               variant="default"
