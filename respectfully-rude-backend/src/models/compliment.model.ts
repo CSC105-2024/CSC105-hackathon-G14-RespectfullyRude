4618f568ce5086a0ab5c9ae8e00c073ed32f8841
Slade <saib6064@gmail.com>
2025-05-31 19:25:07 +0700

    feat(compliment.model.ts): implement CRUD operations for backhanded compliments
    This commit introduces the compliment.model.ts file, which defines
    functions for creating, reading, updating, and deleting (CRUD)
    backhanded compliments in the database. These functions interact with
    the Prisma client to perform the necessary database operations.
    Specifically, the following functions are implemented:
    
    - createList: Creates a new backhanded compliment entry in the database.
    - findImgId: Retrieves the image ID of a specific backhanded compliment.
    - updateList: Updates an existing backhanded compliment entry in the database.

---
+++ respectfully-rude-backend/src/models/compliment.model.ts
@@ -0,0 +1,69 @@
+import { db } from "../index.ts";
+
+export const createList = async (
+  name: string,
+  img_url: string,
+  img_id: string,
+  text: string,
+  backhanded_name: string,
+  backhanded_text: string,
+  backhanded_img: string,
+  user_id: number
+) => {
+  const info = await db.backhandedCompliment.create({
+    data: {
+      user_id: user_id,
+      name: name,
+      img_url: img_url,
+      img_id: img_id,
+      text: text,
+      backhanded_name: backhanded_name,
+      backhanded_text: backhanded_text,
+      backhanded_img: backhanded_img,
+    },
+  });
+  return info;
+};
+
+export const findImgId = async (id: number, user_id: number) => {
+  const info = await db.backhandedCompliment.findUnique({
+    where: {
+      id: id,
+      user_id: user_id,
+    },
+    select: {
+      img_id: true,
+    },
+  });
+
+  return info?.img_id;
+};
+
+export const updateList = async (
+  id: number,
+  name: string,
+  img_url: string,
+  img_id: string,
+  text: string,
+  backhanded_name: string,
+  backhanded_text: string,
+  backhanded_img: string,
+  user_id: number
+) => {
+  const info = await db.backhandedCompliment.update({
+    where: {
+      id: id,
+    },
+    data: {
+      user_id,
+      name,
+      img_url,
+      img_id,
+      text,
+      backhanded_name,
+      backhanded_text,
+      backhanded_img,
+    },
+  });
+  return info;
+};
